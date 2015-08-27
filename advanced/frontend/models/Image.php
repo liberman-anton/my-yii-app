8cafca539e4b2330194c0ed78c0b3af372a88648
liberman-anton <antonliberman@gmail.com>
2015-08-27 11:33:22 +0600

    begin

---
+++ advanced/frontend/models/Image.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 24.08.2015
+ * Time: 11:18
+ */
+
+namespace frontend\models;
+
+
+use yii\base\Model;
+
+class Image extends Model {
+
+    public static function getImageUrl() {
+
+        return "image.png";
+    }
+
+} 
