34414f2e00850cc6a04f2e0b3539a71b59cd8c10
liberman-anton <antonliberman@gmail.com>
2015-08-31 11:23:29 +0600

    setAlias псевдонимы путей

--- advanced/frontend/config/main.php
+++ advanced/frontend/config/main.php
@@ -6,12 +6,42 @@ $params = array_merge(
     require(__DIR__ . '/params-local.php')
 );
 
+Yii::setAlias('@test', '@frontend/test');
+
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+
+    'modules' => [
+        'main' => [
+            'class' => 'app\modules\main\Module',
+        ],
+    ],
+
+
     'components' => [
+
+
+
+                'mail' => [
+                    'class'            => 'zyx\phpmailer\Mailer',
+                    'viewPath'         => '@common/mail',
+                    'useFileTransport' => false,
+                    'config'           => [
+                        'mailer'     => 'smtp',
+                        'host'       => 'smtp.yandex.ru',
+                        'port'       => '465',
+                        'smtpsecure' => 'ssl',
+                        'smtpauth'   => true,
+                        'username'   => 'liberman.anton',
+                        'password'   => '29091992akm',
+                    ],
+                ],
+
+
+
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
