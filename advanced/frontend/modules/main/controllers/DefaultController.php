36de38fb66f4c5e3743e92d069e7c71a80d80621
liberman-anton <antonliberman@gmail.com>
2015-08-31 11:21:12 +0600

    getAlias псевдонимы путей

---
+++ advanced/frontend/modules/main/controllers/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\modules\main\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        $this->layout = "bootstrap";
+
+        return $this->render('index');
+    }
+
+    public function actionPath(){
+        // псевдонимы путей
+        //@yii -путь до папки с фреймворком
+        //@app -путь до текущей активеой папки)(frontend/backend)
+        //@runtime -//-
+        //@webroot -frontend/web или backend/web
+        //@web -url до теущей корневой папки web
+        //@vender
+        //@bower -in vendor
+        //@npm -//-
+        //@frontend
+        //@backend
+
+        print \Yii::getAlias('@webroot');
+
+    }
+}
